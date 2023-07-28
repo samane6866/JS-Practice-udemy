cfbd88ddc78309592aec6f843488c4b8fe2037de
samane6866 <samane.bahramian@gmail.com>
2023-07-28 11:41:52 +0200

    fundamental js

---
+++ index.js
@@ -0,0 +1,7 @@
+// type coercion happen when two oprators has diffrents types
+
+
+// in this example we have one string and one number the result will be string "11",
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
---
+++ index1.js
@@ -0,0 +1,9 @@
+// conditional oprator (Ternary)
+//  Ternary condition has tree part, conditional part- if and else.
+
+const age = 23
+
+age >= 18 ? console.log("I like to drink wine") : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+
