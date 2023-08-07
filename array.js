b838e09524d8d9b72def5cbe9d32dcfbabe5e097
samane6866 <samane.bahramian@gmail.com>
2023-08-07 11:04:57 +0200

    exercise array-function done

---
+++ array.js
@@ -0,0 +1,10 @@
+// remove element from  the end of the array 
+const friends = ["Michel", "Steven", "Sara"];
+
+friends.pop()
+
+
+// add an element to the first of array
+
+const shifted=friends.shift()
+console.log(shifted)
--- index3.js
+++ index3.js
@@ -46,3 +46,26 @@ function fruittProcessor(apples, oranges) {
 }
 
 console.log(fruittProcessor(2, 4));
+
+
+
+
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+
+// A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+
+
+const calAverage = (a, b, c) => (a + b + c) / 3;
+
+let scoreDolphins = console.log(calAverage(44, 23, 71));
+let scoreKoalas = console.log(calAverage(65, 54, 49));
+
+function checkWinner(avgDolpins,avgKoalas){
+    
+    if (avgDolpins >= * 2 avgKoalas) {
+        console.log(`Dolphins win`)
+    }
+}
