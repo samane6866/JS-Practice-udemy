21fef6e4c3077352bbf9967123d96710ff5d5e0c
samane6866 <samane.bahramian@gmail.com>
2023-08-08 17:34:14 +0200

    objects

---
+++ object.js
@@ -0,0 +1,14 @@
+
+
+const jonas = {
+    firstName: "Jonas",
+    lastName: "Schmeteedn",
+    age: 2037 - 1991,
+    friends: ["Michael", "Peter", "Steven"]
+};
+
+
+
+let challenge = console.log(`${jonas["firstName"]} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+console.log(challenge);
